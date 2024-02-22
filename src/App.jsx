039ed32156a40732124a32202db9a0a3d51d4918
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-02-22 17:09:05 +0530

    footer page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
@@ -11,6 +8,7 @@ function App() {
         <main>
           <Outlet />
         </main>
+        <footer className="app__footer"></footer>
       </div>
     </>
   );
